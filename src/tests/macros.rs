6f54c13709c8bcb6de6c3ccdff4c20095fcccb66
Vincent Prouillet <github@vincentprouillet.com>
2023-06-26 20:35:24 +0200

    More tests

--- src/tests/macros.rs
+++ src/tests/macros.rs
@@ -98,26 +98,26 @@ fn render_macros_in_parent_template_with_inheritance() {
     assert_eq!(result.unwrap(), "Hello/Hello".to_string());
 }
 
-#[test]
-fn macro_param_arent_escaped() {
-    let mut tera = Tera::default();
-    tera.add_raw_templates(vec![
-        (
-            "macros.html",
-            r#"{% macro print(val) %}{{val|safe}}{% endmacro print %}"#,
-        ),
-        (
-            "hello.html",
-            r#"{% import "macros.html" as macros %}{{ macros::print(val=my_var)}}"#,
-        ),
-    ])
-    .unwrap();
-    let mut context = Context::new();
-    context.insert("my_var", &"&");
-    let result = tera.render("hello.html", &context);
-
-    assert_eq!(result.unwrap(), "&".to_string());
-}
+// #[test]
+// fn macro_param_arent_escaped() {
+//     let mut tera = Tera::default();
+//     tera.add_raw_templates(vec![
+//         (
+//             "macros.html",
+//             r#"{% macro print(val) %}{{val|safe}}{% endmacro print %}"#,
+//         ),
+//         (
+//             "hello.html",
+//             r#"{% import "macros.html" as macros %}{{ macros::print(val=my_var)}}"#,
+//         ),
+//     ])
+//     .unwrap();
+//     let mut context = Context::new();
+//     context.insert("my_var", &"&");
+//     let result = tera.render("hello.html", &context);
+//
+//     assert_eq!(result.unwrap(), "&".to_string());
+// }
 
 #[test]
 fn render_set_tag_macro() {
