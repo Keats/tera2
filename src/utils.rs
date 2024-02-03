696e3adfa5ffcb0e118b1aa38f96c6c4acca3000
Vincent Prouillet <github@vincentprouillet.com>
2024-02-03 21:39:03 +0100

    Add faster HTML escape behind a feature

--- src/utils.rs
+++ src/utils.rs
@@ -100,18 +100,29 @@ impl Span {
 /// ```
 #[inline]
 pub fn escape_html(input: &str) -> String {
-    let mut output = String::with_capacity(input.len() * 2);
-    for c in input.chars() {
-        match c {
-            '&' => output.push_str("&amp;"),
-            '<' => output.push_str("&lt;"),
-            '>' => output.push_str("&gt;"),
-            '"' => output.push_str("&quot;"),
-            '\'' => output.push_str("&#39;"),
-            _ => output.push(c),
-        }
+    #[cfg(feature = "fast_escape")]
+    {
+        let mut value = String::with_capacity(input.len() + 30);
+        pulldown_cmark_escape::escape_html(&mut value, input)
+            .expect("writing to a string is infallible");
+        value
     }
 
-    // Not using shrink_to_fit() on purpose
-    output
+    #[cfg(not(feature = "fast_escape"))]
+    {
+        let mut output = String::with_capacity(input.len() * 2);
+        for c in input.chars() {
+            match c {
+                '&' => output.push_str("&amp;"),
+                '<' => output.push_str("&lt;"),
+                '>' => output.push_str("&gt;"),
+                '"' => output.push_str("&quot;"),
+                '\'' => output.push_str("&#39;"),
+                _ => output.push(c),
+            }
+        }
+
+        // Not using shrink_to_fit() on purpose
+        output
+    }
 }
