cb6799a6580b543c05d8216dcef301950b7b6b7f
Vincent Prouillet <github@vincentprouillet.com>
2026-01-22 20:39:42 +0100

    u128/i128 keys and handle error when serializing

--- tera/src/value/ser.rs
+++ tera/src/value/ser.rs
@@ -263,7 +263,207 @@ impl ser::SerializeTupleVariant for SerializeTupleVariant {
 
 pub struct SerializeMap {
     entries: Map,
-    key: Option<Value>,
+    key: Option<Key<'static>>,
+}
+
+struct MapKeySerializer;
+
+impl Serializer for MapKeySerializer {
+    type Ok = Key<'static>;
+    type Error = SerializationFailed;
+
+    type SerializeSeq = ser::Impossible<Key<'static>, SerializationFailed>;
+    type SerializeTuple = ser::Impossible<Key<'static>, SerializationFailed>;
+    type SerializeTupleStruct = ser::Impossible<Key<'static>, SerializationFailed>;
+    type SerializeTupleVariant = ser::Impossible<Key<'static>, SerializationFailed>;
+    type SerializeMap = ser::Impossible<Key<'static>, SerializationFailed>;
+    type SerializeStruct = ser::Impossible<Key<'static>, SerializationFailed>;
+    type SerializeStructVariant = ser::Impossible<Key<'static>, SerializationFailed>;
+
+    fn serialize_bool(self, v: bool) -> Result<Self::Ok, Self::Error> {
+        Ok(Key::Bool(v))
+    }
+
+    fn serialize_i8(self, v: i8) -> Result<Self::Ok, Self::Error> {
+        self.serialize_i64(v as i64)
+    }
+
+    fn serialize_i16(self, v: i16) -> Result<Self::Ok, Self::Error> {
+        self.serialize_i64(v as i64)
+    }
+
+    fn serialize_i32(self, v: i32) -> Result<Self::Ok, Self::Error> {
+        self.serialize_i64(v as i64)
+    }
+
+    fn serialize_i64(self, v: i64) -> Result<Self::Ok, Self::Error> {
+        Ok(Key::I64(v))
+    }
+
+    fn serialize_i128(self, v: i128) -> Result<Self::Ok, Self::Error> {
+        Ok(Key::I128(v))
+    }
+
+    fn serialize_u8(self, v: u8) -> Result<Self::Ok, Self::Error> {
+        self.serialize_u64(v as u64)
+    }
+
+    fn serialize_u16(self, v: u16) -> Result<Self::Ok, Self::Error> {
+        self.serialize_u64(v as u64)
+    }
+
+    fn serialize_u32(self, v: u32) -> Result<Self::Ok, Self::Error> {
+        self.serialize_u64(v as u64)
+    }
+
+    fn serialize_u64(self, v: u64) -> Result<Self::Ok, Self::Error> {
+        Ok(Key::U64(v))
+    }
+
+    fn serialize_u128(self, v: u128) -> Result<Self::Ok, Self::Error> {
+        Ok(Key::U128(v))
+    }
+
+    fn serialize_f32(self, _v: f32) -> Result<Self::Ok, Self::Error> {
+        Err(SerializationFailed(
+            "map key must be string, integer, or bool".to_string(),
+        ))
+    }
+
+    fn serialize_f64(self, _v: f64) -> Result<Self::Ok, Self::Error> {
+        Err(SerializationFailed(
+            "map key must be string, integer, or bool".to_string(),
+        ))
+    }
+
+    fn serialize_char(self, v: char) -> Result<Self::Ok, Self::Error> {
+        let mut buf = [0u8; 4];
+        let s = v.encode_utf8(&mut buf);
+        Ok(Key::String(Arc::from(s)))
+    }
+
+    fn serialize_str(self, v: &str) -> Result<Self::Ok, Self::Error> {
+        Ok(Key::String(Arc::from(v)))
+    }
+
+    fn serialize_bytes(self, _v: &[u8]) -> Result<Self::Ok, Self::Error> {
+        Err(SerializationFailed(
+            "map key must be string, integer, or bool".to_string(),
+        ))
+    }
+
+    fn serialize_none(self) -> Result<Self::Ok, Self::Error> {
+        Err(SerializationFailed(
+            "map key must be string, integer, or bool".to_string(),
+        ))
+    }
+
+    fn serialize_some<T: Serialize + ?Sized>(self, value: &T) -> Result<Self::Ok, Self::Error> {
+        value.serialize(self)
+    }
+
+    fn serialize_unit(self) -> Result<Self::Ok, Self::Error> {
+        Err(SerializationFailed(
+            "map key must be string, integer, or bool".to_string(),
+        ))
+    }
+
+    fn serialize_unit_struct(self, _name: &'static str) -> Result<Self::Ok, Self::Error> {
+        Err(SerializationFailed(
+            "map key must be string, integer, or bool".to_string(),
+        ))
+    }
+
+    fn serialize_unit_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        variant: &'static str,
+    ) -> Result<Self::Ok, Self::Error> {
+        Ok(Key::Str(variant))
+    }
+
+    fn serialize_newtype_struct<T: Serialize + ?Sized>(
+        self,
+        _name: &'static str,
+        value: &T,
+    ) -> Result<Self::Ok, Self::Error> {
+        value.serialize(self)
+    }
+
+    fn serialize_newtype_variant<T: Serialize + ?Sized>(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _value: &T,
+    ) -> Result<Self::Ok, Self::Error> {
+        Err(SerializationFailed(
+            "map key must be string, integer, or bool".to_string(),
+        ))
+    }
+
+    fn serialize_seq(self, _len: Option<usize>) -> Result<Self::SerializeSeq, Self::Error> {
+        Err(SerializationFailed(
+            "map key must be string, integer, or bool".to_string(),
+        ))
+    }
+
+    fn serialize_tuple(self, _len: usize) -> Result<Self::SerializeTuple, Self::Error> {
+        Err(SerializationFailed(
+            "map key must be string, integer, or bool".to_string(),
+        ))
+    }
+
+    fn serialize_tuple_struct(
+        self,
+        _name: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeTupleStruct, Self::Error> {
+        Err(SerializationFailed(
+            "map key must be string, integer, or bool".to_string(),
+        ))
+    }
+
+    fn serialize_tuple_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeTupleVariant, Self::Error> {
+        Err(SerializationFailed(
+            "map key must be string, integer, or bool".to_string(),
+        ))
+    }
+
+    fn serialize_map(self, _len: Option<usize>) -> Result<Self::SerializeMap, Self::Error> {
+        Err(SerializationFailed(
+            "map key must be string, integer, or bool".to_string(),
+        ))
+    }
+
+    fn serialize_struct(
+        self,
+        _name: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeStruct, Self::Error> {
+        Err(SerializationFailed(
+            "map key must be string, integer, or bool".to_string(),
+        ))
+    }
+
+    fn serialize_struct_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeStructVariant, Self::Error> {
+        Err(SerializationFailed(
+            "map key must be string, integer, or bool".to_string(),
+        ))
+    }
 }
 
 impl ser::SerializeMap for SerializeMap {
@@ -271,16 +471,15 @@ impl ser::SerializeMap for SerializeMap {
     type Error = SerializationFailed;
 
     fn serialize_key<T: Serialize + ?Sized>(&mut self, key: &T) -> Result<(), Self::Error> {
-        let key = key.serialize(ValueSerializer)?;
+        let key = key.serialize(MapKeySerializer)?;
         self.key = Some(key);
         Ok(())
     }
 
     fn serialize_value<T: Serialize + ?Sized>(&mut self, value: &T) -> Result<(), Self::Error> {
         let key = self.key.take().expect("missing key");
-        let s = key.as_str().expect("key is not string");
         let value = value.serialize(ValueSerializer)?;
-        self.entries.insert(Key::String(Arc::from(s)), value);
+        self.entries.insert(key, value);
         Ok(())
     }
 
@@ -338,5 +537,40 @@ impl ser::SerializeStructVariant for SerializeStructVariant {
             ValueInner::Map(Arc::new(self.fields)).into(),
         );
         Ok(ValueInner::Map(Arc::new(map)).into())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashMap;
+
+    fn get_map_value<'a>(value: &'a Value, key: Key<'a>) -> Option<&'a str> {
+        value.as_map()?.get(&key)?.as_str()
+    }
+
+    #[test]
+    fn bool_and_int_map_keys() {
+        let value = Value::from_serializable(&HashMap::from([(true, "yes")]));
+        assert_eq!(get_map_value(&value, Key::Bool(true)), Some("yes"));
+
+        let value = Value::from_serializable(&HashMap::from([(-5i64, "neg")]));
+        assert_eq!(get_map_value(&value, Key::I64(-5)), Some("neg"));
+
+        let value = Value::from_serializable(&HashMap::from([(7u64, "pos")]));
+        assert_eq!(get_map_value(&value, Key::U64(7)), Some("pos"));
+
+        let value = Value::from_serializable(&HashMap::from([(i128::MIN, "min")]));
+        assert_eq!(get_map_value(&value, Key::I128(i128::MIN)), Some("min"));
+
+        let value = Value::from_serializable(&HashMap::from([(u128::MAX, "max")]));
+        assert_eq!(get_map_value(&value, Key::U128(u128::MAX)), Some("max"));
+    }
+
+    #[test]
+    fn float_map_key_fails() {
+        let result = 3.14f64.serialize(MapKeySerializer);
+        assert!(result.is_err());
+        assert!(result.unwrap_err().to_string().contains("map key must be"));
     }
 }
